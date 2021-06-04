b3b1447e5c418024f2e335a2c3ec07bd5d1a6ab5
Vladimir Ovsyannikov <ovsyannikov_vladimir@mail.ru>
2021-06-04 21:22:18 +0700

    init

---
+++ src/main/java/com/example/purchaseplanner/PurchasePlannerApplication.java
@@ -0,0 +1,13 @@
+package com.example.purchaseplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchasePlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PurchasePlannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/purchaseplanner/PurchasePlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.purchaseplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchasePlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
